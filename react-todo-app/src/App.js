b132e78a5e79e0323f891630f2a9439fa65df862
abh <themechanic002@gmail.com>
2022-07-19 16:05:38 +0900

    App.js edit

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,3 +1,22 @@
+// 클래스형 컴포넌트
+import React, {Component} from "react";
+
+export default class App extends Component {
+  render() {
+    return(
+      <div className="container">
+        <div className="todoBlock">
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+// 함수형 컴포넌트
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,3 +29,4 @@ function App() {
 }
 
 export default App;
+ */
