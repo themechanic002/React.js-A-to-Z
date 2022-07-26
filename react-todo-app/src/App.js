3ccc9523a007552e9e0c8a309f2c54032f0f409c
abh <themechanic002@gmail.com>
2022-07-26 18:56:07 +0900

    new file added

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,18 +1,13 @@
 // 클래스형 컴포넌트
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Form from "./components/Form";
 import List from "./components/List";
 
 export default function App() {
 
   const [todoData, setTodoData] = useState([]);
   const [value, setValue] = useState("");
-
-
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  }
 
   const handleSubmit = (e) => {
     // 입력 버튼 클릭 시 페이지가 새로고침 되는 걸 막아줌
@@ -26,12 +21,11 @@ export default function App() {
     };
 
     // 원래 있던 데이터 리스트에 새로운 항목 추가
-    setTodoData(prev=> [...prev, newTodo]);
+    setTodoData(prev => [...prev, newTodo]);
 
     // 입력 버튼 누르면 글 입력부분 초기화
     setValue("");
   }
-
 
 
   return (
@@ -42,26 +36,10 @@ export default function App() {
         </div>
 
 
-        <List todoData={todoData} setTodoData={setTodoData}/>
+        <List todoData={todoData} setTodoData={setTodoData} />
 
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
 
-        <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="value"
-            style={{ flex: '10', padding: '5px' }}
-            placeholder="해야 할 일을 입력하세요."
-            value={value}
-            onChange={handleChange}
-          />
-          <input
-            type="submit"
-            value="입력"
-            className="btn"
-            style={{ flex: '1' }}
-          />
-
-        </form>
 
       </div>
     </div>
