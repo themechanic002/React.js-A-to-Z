022f78c82096d27c0446cd052cf527476a96cb7a
abh <themechanic002@gmail.com>
2022-07-27 09:22:59 +0900

    Drag and Drop added

--- react-todo-app/src/components/List.js
+++ react-todo-app/src/components/List.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
 export default function List({ todoData, setTodoData }) {
 
@@ -24,29 +25,62 @@ export default function List({ todoData, setTodoData }) {
         setTodoData(newTodoData);
     }
 
+    const handleEnd = (result) => {
+        if(!result.destination) return;
+
+        const newTodoData = todoData;
+
+        const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+
+        newTodoData.splice(result.destination.index, 0, reorderedItem);
+
+        setTodoData(newTodoData);
+    }
+
     return (
 
         /* Map 메소드 사용하는 방법 */
         <div>
-            {todoData.map((data) => (
-                <div key={data.id}>
-                    <div className='flex items-center justify-between w-full px-4 py-1 my2 text-gray-600 bg-gray-100 border rounded'>
-                        <div className='items-center'>
-                            <input
-                                type="checkbox"
-                                defaultChecked={data.completed}
-                                onChange={() => handleCompletedChange(data.id)}
-                            />
-                            <span className= {' pl-2 ' + (data.completed ? 'line-through' : undefined)}>
-                                {data.title}
-                            </span>
+            <DragDropContext onDragEnd={handleEnd}>
+                <Droppable droppableId="todo">
+                    {(provided) => (
+                        <div {...provided.droppableProps} ref={provided.innerRef}>
+                            {todoData.map((data, index) => (
+                                <Draggable
+                                    key={data.id}
+                                    draggableId={data.id.toString()}
+                                    index={index}
+                                >
+                                    {(provided, snapshot) => (
+                                        <div
+                                            key={data.id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}
+                                            className={(snapshot.isDragging ? 'bg-gray-400' : 'bg-gray-100') + ' flex items-center justify-between w-full px-4 py-1 my2 text-gray-600 bg-gray-100 border rounded '}
+                                        >
+
+                                            <div className='items-center'>
+                                                <input
+                                                    type="checkbox"
+                                                    defaultChecked={data.completed}
+                                                    onChange={() => handleCompletedChange(data.id)}
+                                                />
+                                                <span className={' pl-2 ' + (data.completed ? 'line-through' : undefined)}>
+                                                    {data.title}
+                                                </span>
+                                            </div>
+                                            <div className='items-center'>
+                                                <button className='px-4 py-2 float-right' onClick={() => handleClick(data.id)}>x</button>
+                                            </div>
+
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
                         </div>
-                        <div className='items-center'>
-                            <button className='px-4 py-2 float-right' onClick={() => handleClick(data.id)}>x</button>
-                        </div>
-                    </div>
-                </div>
-            ))}
+                    )}
+
+                </Droppable>
+            </DragDropContext>
         </div>
 
 
