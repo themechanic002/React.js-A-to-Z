28c6885208c202c1bcc966ded6714ee34961a253
abh <themechanic002@gmail.com>
2022-08-04 22:43:11 +0900

    netflix starts

--- react-netflix-clone/src/api/axios.js
+++ react-netflix-clone/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "",
+    api_key: "b5d047247c307946228fd63dc22c7030",
     language: 'ko-KR',
   }
 });
---
+++ react-netflix-clone/src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+      <img
+        alt='Netflix logo'
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/220px-Netflix_2015_logo.svg.png'
+        className='nav__logo'
+        onClick={() => window.location.href='https://www.netflix.com/kr/'}
+      />
+      <img
+        alt='User logged'
+        src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+        className='nav__avatar'
+      />
+    </nav>
+  )
+}
+
+export default Nav
