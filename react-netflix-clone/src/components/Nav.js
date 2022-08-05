4de77372ed85551c58b3c606fdb74745ea60c896
abh <themechanic002@gmail.com>
2022-08-05 19:13:55 +0900

    nav animation added

--- react-netflix-clone/src/components/Nav.js
+++ react-netflix-clone/src/components/Nav.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Nav.css'
 
 const Nav = () => {
+
+  const [show, setShow] = useState(false);
+  
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if(window.scrollY > 50) {
+         setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    
+  
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    }
+  }, []);
+
+
   return (
-    <nav className='nav'>
+    <nav className={'nav ' + (show && 'nav__black')}>
       <img
         alt='Netflix logo'
         src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/220px-Netflix_2015_logo.svg.png'
