15dab950bbd9b0e762e687d34380e2ff9e3f979d
abh <themechanic002@gmail.com>
2022-08-05 18:36:03 +0900

    nav basic css

--- react-netflix-clone/src/components/Nav.js
+++ react-netflix-clone/src/components/Nav.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Nav.css'
 
 const Nav = () => {
   return (
-    <nav>
+    <nav className='nav'>
       <img
         alt='Netflix logo'
         src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/220px-Netflix_2015_logo.svg.png'
