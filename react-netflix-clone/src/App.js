754030d21197d31d29e8663a108bc5e433c18c46
abh <themechanic002@gmail.com>
2022-08-09 23:21:16 +0900

    isLargeRow fixed

--- react-netflix-clone/src/App.js
+++ react-netflix-clone/src/App.js
@@ -16,35 +16,35 @@ function App() {
         title="NETFLIX ORIGINALS"
         id="NO"
         fetchURL={requests.fetchNetflixOriginals}
-        isLargeRow="true"
+        isLargeRow={true}
       />
 
       <Row
         title="Trending Now"
         id="TN"
         fetchURL={requests.fetchTrending}
-        isLargeRow="false"
+        isLargeRow={false}
       />
 
       <Row
         title="Top Rated"
         id="TR"
         fetchURL={requests.fetchTopRated}
-        isLargeRow="false"
+        isLargeRow={false}
       />
 
       <Row
         title="Action Movies"
         id="AM"
         fetchURL={requests.fetchActionMovies}
-        isLargeRow="false"
+        isLargeRow={false}
       />
 
       <Row
         title="Comedy Movies"
         id="CM"
         fetchURL={requests.fetchComedyMovies}
-        isLargeRow="false"
+        isLargeRow={false}
       />
 
       <Footer></Footer>
