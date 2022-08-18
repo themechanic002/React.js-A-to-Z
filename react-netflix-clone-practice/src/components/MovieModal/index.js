c02b53eefe53bd619f6154e6e212de989124314d
abh <themechanic002@gmail.com>
2022-08-19 00:21:18 +0900

    Movie Modal added

--- react-netflix-clone-practice/src/components/MovieModal/index.js
+++ react-netflix-clone-practice/src/components/MovieModal/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import './MovieModal.css'
 
-const MovieModal = () => {
+const MovieModal = ({ id, title, backdrop_path, video, first_air_date, release_date, vote_average, overview, setModalOpen }) => {
   return (
     <div className='presentation'>
       <div className='wrapper-modal'>
         <div className='modal'>
-          <span className='modal-close'>X</span>
+          <span className='modal-close' onClick={() => {setModalOpen(false)}}>X</span>
           <img
             className='modal__poster-img'
+            src={`https://image.tmdb.org/t/p/original${backdrop_path}`}
+            alt='title'
           />
           <div className='modal__content'>
             <p className='modal__details'>
@@ -17,9 +20,9 @@ const MovieModal = () => {
               {release_date ? release_date : first_air_date}
             </p>
 
-            <h2 className='modal__title'></h2>
+            <h2 className='modal__title'>{title}</h2>
             <p className='modal__overview'></p>
-            <p className='modal__overview'></p>
+            <p className='modal__overview'>{overview}</p>
           </div>
         </div>
 
--- react-netflix-clone-practice/src/components/Row.js
+++ react-netflix-clone-practice/src/components/Row.js
@@ -1,10 +1,13 @@
 import axios from '../api/axios';
 import React, { useEffect, useState } from 'react'
 import './Row.css';
+import MovieModal from './MovieModal/index';
 
 const Row = ({ title, isLarge, fetchUrl }) => {
 
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
 
   useEffect(() => {
     getData();
@@ -13,12 +16,13 @@ const Row = ({ title, isLarge, fetchUrl }) => {
   const getData = async () => {
     const request = await axios.get(fetchUrl);
     setMovies(request.data.results);
-    console.log('movie', movies)
+    console.log('movie', request);
   }
 
   const arrowRight = () => {
     document.getElementById(`posters_${title}`).scrollLeft += 800;
   }
+
   const arrowLeft = () => {
     document.getElementById(`posters_${title}`).scrollLeft -= 800;
   }
@@ -41,6 +45,10 @@ const Row = ({ title, isLarge, fetchUrl }) => {
               className={`row__poster ${isLarge && "row__posterLarge"}`}
               src={`https://image.tmdb.org/t/p/original${isLarge? movie.poster_path : movie.backdrop_path}`}
               alt={movie.title}
+              onClick={() => {
+                setModalOpen(true);
+                setMovieSelected(movie);
+              }}
               />
             ))
           }
@@ -50,6 +58,13 @@ const Row = ({ title, isLarge, fetchUrl }) => {
         </div>
 
       </div>
+      
+      {modalOpen && (
+        <MovieModal
+          {...movieSelected}
+          setModalOpen = {setModalOpen}
+        />
+      )}
 
     </section>
   )
