6e00e5813acff9b3043b2280882d6aec25235d82
abh <themechanic002@gmail.com>
2022-08-26 18:34:49 +0900

    search query

--- react-netflix-clone/src/pages/SearchPage/index.js
+++ react-netflix-clone/src/pages/SearchPage/index.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import axios from '../../api/axios';
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const SearchPage = () => {
+
+  const [searchResults, setSearchResults] = useState([]);
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  }
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if(searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm])
+  
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`);
+      console.log(request.data.results);
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
+
+
   return (
     <div>SearchPage</div>
   )
